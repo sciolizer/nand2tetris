89614863f4ac01148d095011289b8edb0789451f
Joshua Ball <joshball@yahoo-inc.com>
2014-10-31 07:41:43 -0700

    First attempt at multiply program.

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,21 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2
+M=0
+(add)
+@R0
+D=M
+@end
+D;JLE
+@R0
+D=M
+@R1
+D=D+M
+@R2
+M=D
+@add
+0;JMP
+(end)
+@end
+0;JMP
